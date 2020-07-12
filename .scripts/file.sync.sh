d5ed5df2875d0c39a777944b79b1eecb93664933
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 16:33:11 +0200

    Change file.sync from exclude rule to filter rule

--- .scripts/file.sync.sh
+++ .scripts/file.sync.sh
@@ -2,11 +2,12 @@
 SRC="$HOME"
 DST="/Volumes/Home/"
 EXCLUDE_FILE="$HOME/dotfiles/.scripts/rsync.excl"
+FILTER_RULE="$HOME/dotfiles/.scripts/filter.rules"
 
 if [ -d  $DST ]
 then
-    echo "---- Sync ----"
-    rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
+    # rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
+    rsync -ravuzhHSP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
 else
     /usr/bin/osascript -e 'display notification "Some problem with file sync!" with title "Sync"'
 fi
