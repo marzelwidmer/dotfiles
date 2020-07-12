a3908da6c3c780d60cbbb3eade69162721063b76
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 10:39:42 +0200

    update rsync fwatch sample

--- .rsync/rsync.sh
+++ .rsync/rsync.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 SRC="$HOME"
 DST="/Volumes/Home/"
-RSYNC_EXCL="$HOME/dotfiles/.scripts/rsync.excl"
+EXCLUDE_FILE="$HOME/dotfiles/.scripts/rsync.excl"
 
 fswatch -0 $SRC | while read -d "" event; do
-    #rsync -rauvzhHSP --delete-after --stats --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rauz --quiet --delete-after --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rauz --quiet --delete-after --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rau --quiet --delete --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    # rsync -rtuv --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    rsync -rau --quiet --delete --exclude-from=$RSYNC_EXCL 
+    # rsync -ravuzhHSP --dry-run --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
+    rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
 done
 
  
