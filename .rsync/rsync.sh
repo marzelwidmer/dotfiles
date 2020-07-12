eb1a1ba1d8e6120d971d469fe210a8714679277e
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 19:58:17 +0200

    Add prozess in background for rsync fswatch

--- .rsync/rsync.sh
+++ .rsync/rsync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SRC="$HOME"
 DST="/Volumes/AirPort"
-EXCLUDE_FILE="$HOME/dotfiles/.scripts/rsync.excl"
+EXCLUDE_FILE="$HOME/dotfiles/.rsync/rsync.excl"
 
 fswatch -0 $SRC | while read -d "" event; do
     # rsync -ravuzhHSP --dry-run --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
     rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
-done
+done &
