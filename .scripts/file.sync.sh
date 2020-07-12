6afdd132d3154ca0ba7fe50097b18f9c4c57252a
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 18:24:31 +0200

    Polishing.

--- .scripts/file.sync.sh
+++ .scripts/file.sync.sh
@@ -7,7 +7,8 @@ FILTER_RULE="$HOME/dotfiles/.scripts/filter.rules"
 if [ -d  $DST ]
 then
     # rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
-    rsync -ravuzhHSP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
+    # rsync -ravuzhHSP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
+    rsync -ravuzhP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
 else
     /usr/bin/osascript -e 'display notification "Some problem with file sync!" with title "Sync"'
 fi
