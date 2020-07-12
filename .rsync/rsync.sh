0cc61462da3c3274dd2fd5c2bfff7e6f93da1c6e
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 21:29:36 +0200

    Polishing.

--- .rsync/rsync.sh
+++ .rsync/rsync.sh
@@ -5,5 +5,5 @@ EXCLUDE_FILE="$HOME/dotfiles/.rsync/rsync.excl"
 
 fswatch -0 $SRC | while read -d "" event; do
     # rsync -ravuzhHSP --dry-run --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
-    rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
+    rsync -ravuzq --delete-after --log-file=/tmp/rsync.log --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST  
 done &
