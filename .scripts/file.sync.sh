1a5a05f198da4078daa3c171dd92eb36b2300afd
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-12 10:56:27 +0200

    Add .gitignore file

--- .scripts/file.sync.sh
+++ .scripts/file.sync.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 SRC="$HOME"
 DST="/Volumes/Home/"
-RSYNC_EXCL="$HOME/dotfiles/.scripts/rsync.excl"
+EXCLUDE_FILE="$HOME/dotfiles/.scripts/rsync.excl"
 
 if [ -d  $DST ]
 then
     echo "---- Sync ----"
-    rsync -rauvzhHSP --delete-after --stats --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rauz --quiet --delete-after --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rauz --quiet --delete-after --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    #rsync -rau --quiet --delete --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
-    # rsync -rtuv --exclude-from=$RSYNC_EXCL "$SRC" "$DST"
+    rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
 else
     /usr/bin/osascript -e 'display notification "Some problem with file sync!" with title "Sync"'
 fi
