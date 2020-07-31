e22d7b4c65ca1ea07a9244e5a88d2f2843f2c7d6
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-31 18:14:10 +0200

    change order of sync

--- .scripts/file.sync.sh
+++ .scripts/file.sync.sh
@@ -9,10 +9,12 @@ if [ -d  $DST ]
 then
     # rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
     # rsync -ravuzhHSP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
-    rsync -ravuzhPE  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
-    /usr/bin/say -v "Oliver" "Hera Sync, is Done"
     rsync -ravuzhPE  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST_AIRPORT
     /usr/bin/say -v "Lee" "AirPort Sync, is Done"
+    
+    rsync -ravuzhPE  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
+    /usr/bin/say -v "Oliver" "Hera Sync, is Done"
+   
 else
     /usr/bin/osascript -e 'display notification "Some problem with file sync!" with title "Sync"'
     /usr/bin/say -v "Good News" "Synchronisation have some problems."
