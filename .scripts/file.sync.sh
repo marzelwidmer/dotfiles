83952d1f0319ca4cf057205fc6995d734175bf04
Marcel Widmer <marzelwidmer@gmail.com>
2020-07-31 17:48:13 +0200

    Add Airport Sync

--- .scripts/file.sync.sh
+++ .scripts/file.sync.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SRC="$HOME"
 DST="/Volumes/Home/"
+DST_AIRPORT="/Volumes/AirPort/"
 EXCLUDE_FILE="$HOME/dotfiles/.scripts/rsync.excl"
 FILTER_RULE="$HOME/dotfiles/.scripts/filter.rules"
 
@@ -9,7 +10,15 @@ then
     # rsync -ravuzhHSP --delete-after --delete-excluded --exclude-from=$EXCLUDE_FILE $SRC $DST 
     # rsync -ravuzhHSP  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
     rsync -ravuzhPE  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST
+    /usr/bin/say Hera Sync is Done
+    rsync -ravuzhPE  --delete-after --delete-excluded  --filter="merge $FILTER_RULE" $SRC $DST_AIRPORT
+    /usr/bin/say AirPort Sync is Done
 else
     /usr/bin/osascript -e 'display notification "Some problem with file sync!" with title "Sync"'
+    /usr/bin/say Some problem with home sync!
 fi
+
+
+
+
 
