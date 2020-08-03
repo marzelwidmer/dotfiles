86b1cc96692f033bff1e0807c2e3a233e8b663ac
Marcel Widmer <marzelwidmer@gmail.com>
2020-08-03 20:14:56 +0200

    remove home synch voice

--- launchd/home-sync/home-sync.sh
+++ launchd/home-sync/home-sync.sh
@@ -9,7 +9,7 @@ then
     rsync -ravuzhPE  --delete-after --exclude "helsana"  --log-file=/tmp/airport-home.log --filter="merge $FILTER_RULE" $SRC $DST_AIRPORT
     rsync -ravuzhPE  --delete-after --exclude "helsana"  --log-file=/tmp/hera-home.log --filter="merge $FILTER_RULE" $SRC $DST
 
-	  /usr/bin/say -v "lee" "Home directory is successfully synchronized."
+#	  /usr/bin/say -v "lee" "Home directory is successfully synchronized."
 
 else
     /usr/bin/osascript -e 'display notification "Some problem with home sync!" with title "Home Sync"'
