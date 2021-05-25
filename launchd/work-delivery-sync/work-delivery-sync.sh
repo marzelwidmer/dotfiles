12a1c094c16065676b6b3e887eeebdfd3809afd0
marzelwidmer <marzelwidmer@gmail.com>
2021-05-25 08:45:56 +0200

    Update sync delivery

--- launchd/work-delivery-sync/work-delivery-sync.sh
+++ launchd/work-delivery-sync/work-delivery-sync.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-SRC="$HOME/helsana/"
-DST="/Volumes/AirPort/morpheus/helsana"
+SRC="$HOME/helsana/azure"
+DST="/Volumes/AirPort/helsana"
 
 if [ -d  $DST ]
 then
 #   	/usr/bin/osascript -e 'display notification "Delivery sync is started" with title "Delivery Sync"'
-   	rsync -az --delete-after --exclude "dev" --exclude "azure" --exclude "alma" --exclude "gitlab"  --log-file=/tmp/work-delivery-sync.log  $SRC $DST
+#   	rsync -az --delete-after --exclude "dev" --exclude "azure" --exclude "alma" --exclude "gitlab"  --log-file=/tmp/work-delivery-sync.log  $SRC $DST
+   	rsync -az --delete-after --log-file=/tmp/work-delivery-sync.log  $SRC $DST
 #    rsync -ravuzhPE --delete-after --delete-excluded --log-file=/tmp/airport-work-delivery.log --filter="merge $FILTER_RULE" $SRC $DST
 #  --dry-run
 # rsync -az --exclude "delivery"  --log-file=/tmp/airport-work-delivery.log  $HOME/helsana/ /Volumes/AirPort/morpheus/helsana
@@ -17,8 +18,3 @@ else
     /usr/bin/osascript -e 'display notification "Some problem with delivery sync!" with title "Delivery Sync"'
 #	  /usr/bin/say -v "Veena" "Holy molly ... Houston we have a problem with synchronisation of delivery directory."
 fi
-
-
-
-
-
